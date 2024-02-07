d313bd1256a9c860a2279814ca08d3bbe5dc620c
Bruno Becoski <brunobecoski@gmail.com>
2024-02-07 15:08:46 -0300

    Adiciona a fonte Inter

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -2,8 +2,10 @@ import { Text, View } from 'react-native';
 
 export default function Home() {
   return (
-    <View className="bg-slate-900 flex-1">
-      <Text className="text-white">React Native</Text>
+    <View className="flex-1 pt-8">
+      <Text className="text-white text-8xl font-heading">
+        React Native!
+      </Text>
     </View>
   )
 }
