9dd1fb18e7265497e03dc9be0f8e26f0f54e1b50
Bruno Becoski <brunobecoski@gmail.com>
2024-02-07 16:18:45 -0300

    Cria os componentes Header e o CategoryButton

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,11 +1,37 @@
-import { Text, View } from 'react-native';
+import { useState } from 'react';
+import { FlatList, View } from 'react-native';
+
+import { Header } from '@/components/header';
+import { CategoryButton } from '@/components/category-button';
+
+import { CATEGORIES } from '@/utils/data/products';
 
 export default function Home() {
+  const [category, setCategory] = useState(CATEGORIES[0]);
+
+  function handleCategorySelect(selectedCategory: string) {
+    setCategory(selectedCategory);
+  }
+
   return (
     <View className="flex-1 pt-8">
-      <Text className="text-white text-8xl font-heading">
-        React Native!
-      </Text>
+      <Header title="Faça seu pedido" cardQuantityItems={5} />
+
+      <FlatList 
+        data={CATEGORIES}
+        keyExtractor={(item) => item}
+        renderItem={({ item }) =>
+          <CategoryButton 
+            title={item}
+            isSelected={item === category }
+            onPress={() => handleCategorySelect(item)}
+          /> 
+        }
+        horizontal
+        className="max-h-10 mt-5"
+        showsHorizontalScrollIndicator={false}
+        contentContainerStyle={{ gap: 12, paddingHorizontal: 20 }}
+      />
     </View>
   )
 }
