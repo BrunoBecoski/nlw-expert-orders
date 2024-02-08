3c6035872489957056742ab96e84569c000d0f15
Bruno Becoski <brunobecoski@gmail.com>
2024-02-08 20:57:25 -0300

    Cria a funcionalidade de pedido

--- src/app/cart.tsx
+++ src/app/cart.tsx
@@ -1,4 +1,6 @@
-import { Alert, ScrollView, Text, View } from 'react-native';
+import { useState } from 'react';
+import { Alert, Linking, ScrollView, Text, View } from 'react-native';
+import { useNavigation } from 'expo-router';  
 import { KeyboardAwareScrollView } from 'react-native-keyboard-aware-scroll-view';
 import { Feather } from '@expo/vector-icons';
 
@@ -10,15 +12,19 @@ import { Input } from '@/components/input';
 import { Button } from '@/components/button';
 import { LinkButton } from '@/components/link-button';
 
+const PHONE_NUMBER = "";
+
 export default function Cart() {
+  const [address, setAddress] = useState('');
   const cartStore = useCartStore();
+  const navigation = useNavigation();
 
   const total = formatCurrency(cartStore.products.reduce((total, product) =>
     total + product.price + product.quantity, 0
   )); 
 
   function handleProductRemove(product: ProductCartProps) {
-    Alert.alert("Remover", `Deseja remover ${product.title} do carrinho?`, [
+    Alert.alert('Remover', `Deseja remover ${product.title} do carrinho?`, [
       { 
         text: 'Cancelar',
       },
@@ -27,6 +33,35 @@ export default function Cart() {
         onPress: () => cartStore.remove(product.id),
       }
     ]);
+  }
+
+  function handleOrder() {
+    if (cartStore.products.length === 0) {
+      return Alert.alert('Pedido', 'Adicione um pedido.');
+    }
+
+    if (address.trim().length === 0) {
+      return Alert.alert('Pedido', 'Informe os dados da entrega.');
+    }
+    
+    const products = cartStore.products
+      .map((product) => `\n ${product.quantity} ${product.title}`)
+      .join('');
+
+    const message = `
+    🍔 NOVO PEDIDO 🍔
+    \n Entregar em: ${address}
+    ${products}
+    \n Valor total: ${total}
+    `;
+
+    // Linking.openURL(`http://api.whatsapp.com/send?phone=${PHONE_NUMBER}&text=${message}`);
+
+    console.log(message);
+
+    cartStore.clear();
+
+    navigation.goBack();
   }
 
   return (
@@ -66,13 +101,18 @@ export default function Cart() {
 
             <Input
               placeholder="Informe o endereço de entrega com rua, bairro, CEP, número e complemento"
+              value={address}
+              onChangeText={setAddress}
+              blurOnSubmit={true}
+              onSubmitEditing={handleOrder}
+              returnKeyType="next"
             />
           </View>
         </ScrollView>
       </KeyboardAwareScrollView>
 
       <View className="p-5 gap-5">
-        <Button>
+        <Button onPress={handleOrder}>
           <Button.Text>Enviar pedido</Button.Text>
           <Button.Icon>
             <Feather name="arrow-right-circle" size={20} />
--- src/stores/cart-store.ts
+++ src/stores/cart-store.ts
@@ -1,4 +1,6 @@
 import { create } from 'zustand';
+import { createJSONStorage, persist } from 'zustand/middleware';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 import * as cartInMemory from './helpers/cart-in-memory';
 import { ProductProps } from '@/utils/data/products';
@@ -11,9 +13,11 @@ interface StateProps {
   products: ProductCartProps[];
   add: (product: ProductProps) => void;
   remove: (productId: string) => void;
+  clear: () => void;
 }
 
-export const useCartStore = create<StateProps>((set) => ({
+export const useCartStore = create(
+  persist<StateProps>((set) => ({
   products: [],
 
   add: (product: ProductProps) => 
@@ -25,4 +29,9 @@ export const useCartStore = create<StateProps>((set) => ({
     set((state) => ({
       products: cartInMemory.remove(state.products, productId)
     })),
+
+  clear: () => set(() => ({ products: [] })),
+}), {
+  name: "nlw-expert:cart",
+  storage: createJSONStorage(() => AsyncStorage)
 }))
